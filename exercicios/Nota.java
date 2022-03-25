e033991b53a622e5e1161ffbdbb911644e3a9f9f
Sara Conce <100882138+Saraconce@users.noreply.github.com>
2022-03-24 23:44:50 -0300

    ExercíciosJava

--- exercicios/Nota.java
+++ exercicios/Nota.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/* Faca um programa que peça um nota, entre zero e dez.
+/* Faça um programa que peça um nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido
 e continue pedindo até que o usuário informe um valor válido.*/
 
