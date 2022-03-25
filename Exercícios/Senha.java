b37a5093d8f266625bfae602fb38c22b952ac4d3
Sara Conce <100882138+Saraconce@users.noreply.github.com>
2022-03-24 23:46:50 -0300

    ExercíciosJava

--- Exercícios/Senha.java
+++ Exercícios/Senha.java
@@ -1,4 +1,5 @@
-/*Faça um programa que leia um nome de usuário e a sua senha
+
+ /*Faça um programa que leia um nome de usuário e a sua senha
 e não aceite a senha igual ao nome do usuário,
 mostrando uma mensagem de erro e voltando a pedir as informações.*/
 
