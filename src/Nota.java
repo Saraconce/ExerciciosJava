acd0e0973ee1893ad5bfe4690860f3b083c045d7
Sara Conce <100882138+Saraconce@users.noreply.github.com>
2022-03-24 21:01:12 -0300

    Initial commit

---
+++ src/Nota.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/* Faca um programa que peça um nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo até que o usuário informe um valor válido.*/
+
+public class Nota {
+
+    public static void main (String []args){
+        Scanner scan= new Scanner(System.in);
+        int nota;
+        System.out.println("Nota: ");
+
+        nota= scan.nextInt();
+        while (nota < 0 | nota >10) {
+            System.out.println("Nota inválida, digite novamente: ");
+            nota= scan.nextInt();
+        }
+    }
+}
